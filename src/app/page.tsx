7be125ee4047d14834a563217c650eb4e324dd56
hafizmp <hafiznjh007@gmail.com>
2023-11-17 14:57:55 +0530

    jest testing setup🛠

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,9 +65,9 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>
+            </span> */}
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
